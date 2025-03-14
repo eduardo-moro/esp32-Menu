fa51347eccf4f27fb371891f6c217e79aeb72039
eduardo moro <dev.eduardomoro@gmail.com>
2025-03-14 17:08:37 -0300

    connecting to wifi

--- sketch_mar10a.ino
+++ sketch_mar10a.ino
@@ -1,7 +1,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
-
+#include <qrcode_st7789.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 #define TFT_DC  2
 #define TFT_RST 4
@@ -22,9 +24,11 @@
 #define TFT_HEIGHT 280
 #define TFT_ROTATION 2
 
-#define BACKGROUND 0x0000
-#define MENU_BACKGROUND 0xFFFF
+#define BACKGROUND 0xFFFF
+#define MENU_BACKGROUND 0x0000
 #define SUB_MENU_BACKGROUND 0x4BF3
+
+#define WD 200
 
 bool lastStateUp = HIGH;
 bool lastStateDown = HIGH;
@@ -43,13 +47,15 @@ void setBrightness(int level);
 void doNothing();
 void adjustBrightnessRight();
 void adjustBrightnessLeft();
+void wifiOnMid();
 
 int brightness = 255;
 bool isMainItemReversed = false;
+int lastStationCount = -1;
 
 /*
- * Menu interface style
- */
+   Menu interface style
+*/
 // border radius
 int radius = 12;
 int item_space = 4;
@@ -74,16 +80,16 @@ int sub_item_bottom_position_y = main_item_position_y + main_item_height + item_
 
 
 typedef struct {
-    const char* label;
-    void (*onMid)();
-    void (*onLeft)();
-    void (*onRight)();
+  const char* label;
+  void (*onMid)();
+  void (*onLeft)();
+  void (*onRight)();
 } MenuItem;
 
 // menu items
 MenuItem menuItems[] = {
-    {"Wifi", doNothing, doNothing, doNothing},
-    {"< Bright >", doNothing, adjustBrightnessLeft, adjustBrightnessRight},
+  {"Wifi", wifiOnMid, doNothing, doNothing},
+  {"< Bright >", doNothing, adjustBrightnessLeft, adjustBrightnessRight},
 };
 
 const int menuItemCount = sizeof(menuItems) / sizeof(menuItems[0]);
@@ -91,6 +97,17 @@ const int menuItemCount = sizeof(menuItems) / sizeof(menuItems[0]);
 int currentMenuIndex = 0;
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+QRcode_ST7789 qrcode (&tft);
+WebServer server(80);
+
+bool apMode = false;
+String wifiSSID;
+String wifiPassword;
+
+void startAP();
+void stopAP();
+void displayQRCode();
+void handleWiFiPage();
 
 void setup () {
   //brightness
@@ -119,6 +136,23 @@ void setup () {
 }
 
 void loop() {
+  if (apMode) {
+    server.handleClient();
+    int currentStationCount = WiFi.softAPgetStationNum();
+
+    // Only update the QR code if the number of connected clients changes
+    if (currentStationCount != lastStationCount) {
+      displayQRCode();
+      lastStationCount = currentStationCount; // Update the last known station count
+    }
+
+    // If the ESP has connected to a WiFi network as a station, stop AP mode.
+    if (WiFi.status() == WL_CONNECTED) {
+      stopAP();
+      initializeMenuLayout();
+    }
+  }
+
   checkButton(UP_BTN, lastStateUp, "Up");
   checkButton(DOWN_BTN, lastStateDown, "Down");
   checkButton(LEFT_BTN, lastStateLeft, "Left");
@@ -128,6 +162,104 @@ void loop() {
   checkButton(RESET_BTN, lastStateReset, "Reset");
 
   delay(50);
+}
+
+void handleConnect() {
+  if (server.hasArg("ssid") && server.hasArg("pass")) {
+    String ssid = server.arg("ssid");
+    String pass = server.arg("pass");
+    // Attempt to connect to the selected WiFi network
+    WiFi.begin(ssid.c_str(), pass.c_str());
+    server.send(200, "text/html", "<html><body><h1>Connecting to " + ssid + "...</h1></body></html>");
+  } else {
+    server.send(400, "text/html", "<html><body><h1>Bad Request</h1></body></html>");
+  }
+}
+
+void wifiOnMid() {
+  if (!apMode) {
+    // In menu mode: start the AP and display the QR code.
+    startAP();
+  } else {
+    // In QR mode: stop AP and return to the menu.
+    stopAP();
+    initializeMenuLayout();
+  }
+}
+
+void startAP() {
+  // Generate a random SSID and password for the ESP AP
+  wifiSSID = "MORO";
+  wifiPassword = "PASS" + String(random(1000, 9999));
+
+  Serial.println("Starting AP: " + wifiSSID);
+
+  WiFi.softAP(wifiSSID.c_str(), wifiPassword.c_str());
+
+  // Setup web server: register both the WiFi page and connect handlers
+  server.on("/", handleWiFiPage);
+  server.on("/connect", HTTP_POST, handleConnect);
+  server.begin();
+
+  apMode = true;
+
+  displayQRCode();
+}
+
+void stopAP() {
+  WiFi.softAPdisconnect(true);
+  server.stop();
+  apMode = false;
+}
+
+void displayQRCode() {
+  tft.fillScreen(BACKGROUND);
+  String qrContent;
+  if (WiFi.softAPgetStationNum() > 0) {
+    // When a client connects to the ESP AP, update the QR code to point to the root ("/") URL.
+    qrContent = "http://" + WiFi.softAPIP().toString();
+    qrcode.init(2, 180, 180);
+    qrcode.create(qrContent.c_str());
+  } else {
+    // Otherwise, display the QR code with the WiFi credentials.
+    String wifiConfig = "WIFI:T:WPA;S:" + wifiSSID + ";P:" + wifiPassword + ";;";
+    qrContent = wifiConfig;
+    
+    qrcode.init(2, 180, 180);
+    qrcode.create(qrContent.c_str());
+    
+    tft.setTextColor(MENU_BACKGROUND);
+    drawCentreString(wifiSSID.c_str(), 32 , 3);
+    drawCentreString(wifiPassword.c_str(), TFT_HEIGHT - 32 , 2);
+  }
+}
+
+void handleWiFiPage() {
+  // Scan for available networks (this may take a moment)
+  int n = WiFi.scanNetworks();
+  String options = "";
+  for (int i = 0; i < n; i++) {
+    // Build an <option> element for each network; you can also display the RSSI if desired
+    options += "<option value='" + WiFi.SSID(i) + "'>" + WiFi.SSID(i) + " (" + String(WiFi.RSSI(i)) + " dBm)</option>";
+  }
+
+  String html = "<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+                "<title>ESP WiFi Setup</title></head><body>";
+  html += "<h1>Connect ESP to WiFi</h1>";
+  html += "<form action='/connect' method='POST'>";
+  html += "<label for='ssid'>Select WiFi Network:</label>";
+  html += "<select name='ssid' id='ssid'>" + options + "</select><br><br>";
+  html += "<label for='pass'>Password:</label>";
+  html += "<input type='password' name='pass' id='pass' placeholder='Password'><br><br>";
+  html += "<input type='submit' value='Connect'>";
+  html += "</form>";
+  // A separate form for re-scanning the available networks.
+  html += "<br><form action='/' method='GET'>";
+  html += "<input type='submit' value='Search Again'>";
+  html += "</form>";
+  html += "</body></html>";
+
+  server.send(200, "text/html", html);
 }
 
 void setBrightness(int level) {
@@ -151,38 +283,42 @@ void doNothing() {
 }
 
 void initializeMenuLayout() {
+  tft.setRotation(TFT_ROTATION);
+  tft.fillScreen(BACKGROUND);
+  tft.setTextColor(MENU_BACKGROUND);
+
   // draw main item
   tft.drawRoundRect(main_item_position_x, main_item_position_y, main_item_width, main_item_height, radius, MENU_BACKGROUND);
 
   // draw sub items
-  tft.drawRoundRect(sub_items_position_x, sub_item_top_position_y,    sub_item_width, sub_item_height, radius, SUB_MENU_BACKGROUND);  
-  tft.drawRoundRect(sub_items_position_x, sub_item_bottom_position_y, sub_item_width, sub_item_height, radius, SUB_MENU_BACKGROUND);  
-  
+  tft.drawRoundRect(sub_items_position_x, sub_item_top_position_y,    sub_item_width, sub_item_height, radius, SUB_MENU_BACKGROUND);
+  tft.drawRoundRect(sub_items_position_x, sub_item_bottom_position_y, sub_item_width, sub_item_height, radius, SUB_MENU_BACKGROUND);
+
   writeMenuItems();
 }
 
 void drawCentreString(const char *buf, int y, int fontSize) {
-    tft.setTextSize(fontSize); // Set font size
+  tft.setTextSize(fontSize); // Set font size
 
-    int16_t x1, y1;
-    uint16_t w, h;
-    tft.getTextBounds(buf, 0, 0, &x1, &y1, &w, &h); // Get text width & height
+  int16_t x1, y1;
+  uint16_t w, h;
+  tft.getTextBounds(buf, 0, 0, &x1, &y1, &w, &h); // Get text width & height
 
-    int x = (tft.width() - w) / 2;  // Center X
-    int yCentered = y - (h / 2);    // Adjust Y to center text properly
+  int x = (tft.width() - w) / 2;  // Center X
+  int yCentered = y - (h / 2);    // Adjust Y to center text properly
 
-    tft.setCursor(x, yCentered);
-    tft.print(buf);
+  tft.setCursor(x, yCentered);
+  tft.print(buf);
 }
 
 void writeMenuItems() {
   int topIndex = (currentMenuIndex - 1 + menuItemCount) % menuItemCount;
   int bottomIndex = (currentMenuIndex + 1) % menuItemCount;
-  
+
   eraseMenuLabels();
-  
+
   drawCentreString(menuItems[currentMenuIndex].label, TFT_HEIGHT / 2, 3);
-  
+
   tft.setTextColor(SUB_MENU_BACKGROUND);
   drawCentreString(menuItems[topIndex].label,    (TFT_HEIGHT / 2) - (main_item_height / 2) - item_space - (sub_item_height / 2), 2);
   drawCentreString(menuItems[bottomIndex].label, (TFT_HEIGHT / 2) + (main_item_height / 2) + item_space + (sub_item_height / 2), 2);
@@ -191,8 +327,8 @@ void writeMenuItems() {
 
 
 /*
- * desenha o item principal com a cor do background, e o item preenchido pela cor principal
- */
+   desenha o item principal com a cor do background, e o item preenchido pela cor principal
+*/
 void reverseMainItem() {
   tft.setTextColor(BACKGROUND);
   tft.fillRoundRect(main_item_position_x, main_item_position_y, main_item_width, main_item_height, radius, MENU_BACKGROUND);
@@ -201,13 +337,13 @@ void reverseMainItem() {
 }
 
 /*
- * desenha o item principal com a cor principal, limpa a área do botão, e então redesenha o botão
- * (não é chamada a função writeMenuItems para evitar piscar a tela.
- */
+   desenha o item principal com a cor principal, limpa a área do botão, e então redesenha o botão
+   (não é chamada a função writeMenuItems para evitar piscar a tela.
+*/
 void redrawMainItem() {
   tft.fillRect(     main_item_position_x, main_item_position_y, main_item_width, main_item_height, BACKGROUND);
   tft.drawRoundRect(main_item_position_x, main_item_position_y, main_item_width, main_item_height, radius, MENU_BACKGROUND);
-  
+
   tft.setTextColor(MENU_BACKGROUND);
   drawCentreString(menuItems[currentMenuIndex].label, TFT_HEIGHT / 2, 3);
 }
@@ -219,43 +355,48 @@ void eraseMenuLabels() {
 }
 
 void checkButton(int button, bool &lastState, const char *name) {
-    bool currentState = digitalRead(button);
+  bool currentState = digitalRead(button);
 
-    if (currentState != lastState) {
-        if (currentState == LOW) {
-            Serial.print("Pressed ");
-            Serial.println(name);
+  if (currentState != lastState) {
+    if (currentState == LOW) {
+      Serial.print("Pressed ");
+      Serial.println(name);
 
-            if (button == RESET_BTN) {
-                initializeMenuLayout();
-            } else if (button == DOWN_BTN) {
-                currentMenuIndex = (currentMenuIndex + 1) % menuItemCount;
-                writeMenuItems();
-            } else if (button == UP_BTN) {
-                currentMenuIndex = (currentMenuIndex - 1 + menuItemCount) % menuItemCount;
-                writeMenuItems();
-            } else if (button == MID_BTN) {
-                menuItems[currentMenuIndex].onMid();
-            } else if (button == LEFT_BTN) {
-                menuItems[currentMenuIndex].onLeft();
-            } else if (button == RIGHT_BTN) {
-                menuItems[currentMenuIndex].onRight();
-            }
-        } else if (currentState == HIGH) {
-            if (button == MID_BTN) {
-              if (menuItems[currentMenuIndex].onMid == doNothing) {
-                redrawMainItem();
-              }
-            } else if (button == LEFT_BTN) {
-              if (menuItems[currentMenuIndex].onLeft == doNothing || menuItems[currentMenuIndex].onLeft == adjustBrightnessLeft) {
-                redrawMainItem();
-              }
-            } else if (button == RIGHT_BTN) {
-              if (menuItems[currentMenuIndex].onRight == doNothing || menuItems[currentMenuIndex].onRight == adjustBrightnessRight) {
-                redrawMainItem();
-              }
-            }
+      if (apMode) {
+        stopAP();
+        initializeMenuLayout();
+      }
+
+      if (button == RESET_BTN) {
+        initializeMenuLayout();
+      } else if (button == DOWN_BTN) {
+        currentMenuIndex = (currentMenuIndex + 1) % menuItemCount;
+        writeMenuItems();
+      } else if (button == UP_BTN) {
+        currentMenuIndex = (currentMenuIndex - 1 + menuItemCount) % menuItemCount;
+        writeMenuItems();
+      } else if (button == MID_BTN) {
+        menuItems[currentMenuIndex].onMid();
+      } else if (button == LEFT_BTN) {
+        menuItems[currentMenuIndex].onLeft();
+      } else if (button == RIGHT_BTN) {
+        menuItems[currentMenuIndex].onRight();
+      }
+    } else if (currentState == HIGH) {
+      if (button == MID_BTN) {
+        if (menuItems[currentMenuIndex].onMid == doNothing) {
+          redrawMainItem();
         }
-        lastState = currentState;
+      } else if (button == LEFT_BTN) {
+        if (menuItems[currentMenuIndex].onLeft == doNothing || menuItems[currentMenuIndex].onLeft == adjustBrightnessLeft) {
+          redrawMainItem();
+        }
+      } else if (button == RIGHT_BTN) {
+        if (menuItems[currentMenuIndex].onRight == doNothing || menuItems[currentMenuIndex].onRight == adjustBrightnessRight) {
+          redrawMainItem();
+        }
+      }
     }
+    lastState = currentState;
+  }
 }
